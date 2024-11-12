a32be358e87cc74643f4d26a34b4b5cb8320f78f
Marco Macias <marcomacias410@csu.fullerton.edu>
2024-11-11 19:09:28 -0800

    removed commented out code

--- redir.c
+++ redir.c
@@ -1,4 +1,5 @@
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,7 +7,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <stdbool.h>
 
 /*Usage: redir <inp> <cmd> <out> */
 
@@ -16,82 +16,57 @@ void add_character_to_string(char *str, char c) {
     str[len + 1] = '\0';
 }
 
-
-
-// splits string by spaces; adds sa nULL into the array after the last word
-void split (char * cmd, char * words[], char delimiter){
- 
-
+void split(char *cmd, char *words[], char delimiter) {
     int word_count = 0;
-    char * next_char = cmd;
+    char *next_char = cmd;
     char current_word[1000];
     strcpy(current_word, "");
 
-
-    while (*next_char != '\0'){
-       
-       if (*next_char == delimiter){
-        words[word_count++]  = strdup(current_word);
-        strcpy(current_word, "");
-       } else {
-        add_character_to_string(current_word, *next_char);
-       }
-       ++next_char;
-
+    while (*next_char != '\0') {
+        if (*next_char == delimiter) {
+            words[word_count++] = strdup(current_word);
+            strcpy(current_word, "");
+        } else {
+            add_character_to_string(current_word, *next_char);
+        }
+        ++next_char;
     }
 
     words[word_count++] = strdup(current_word);
     words[word_count] = NULL;
 }
 
-
-void break_into_words(char * cmd, char *words[]){
-
-  
+void break_into_words(char *cmd, char *words[]) {
     words[0] = strdup(cmd);
     words[1] = NULL;
 }
 
-bool find_absolute_path( char * cmd, char *absolute_path){
-  
-    
-      char * directories [1000];
+bool find_absolute_path(char *cmd, char *absolute_path) {
+    char *directories[1000];
 
-      split(getenv("PATH"), directories, ':');
-      // look in array until I find the paththing+cmd
+    split(getenv("PATH"), directories, ':');
 
-      for (int ix = 0; directories[ix] != NULL; ix++){
-
+    for (int ix = 0; directories[ix] != NULL; ix++) {
         char path[1000];
         strcpy(path, directories[ix]);
         add_character_to_string(path, '/');
         strcat(path, cmd);
 
-        if (access(path, X_OK) == 0){
+        if (access(path, X_OK) == 0) {
             strcpy(absolute_path, path);
             return true;
         }
-
-
-      }
-
+    }
 
     return false;
-
 }
 
-
-
-
 int main(int argc, char *argv[]) {
-
     char absolute_path[1000];
     char *words[1000];
 
     int fd_input;
     int fd_output;
-
-
 
     if (argc < 4) {
         fprintf(stderr, "Usage %s <inp> <cmd> <out> \n", argv[0]);
@@ -99,7 +74,7 @@ int main(int argc, char *argv[]) {
     }
     split(argv[2], words, ' ');
 
-    if(words[0] == NULL){
+    if (words[0] == NULL) {
         printf("No command found\n");
         return 1;
     }
@@ -108,8 +83,6 @@ int main(int argc, char *argv[]) {
         printf("Command not found: %s\n", words[0]);
         return 1;
     }
-
-
 
     printf("argc %d", argc);
 
@@ -125,8 +98,7 @@ int main(int argc, char *argv[]) {
         close(fd_input);
     }
 
-
-    // Check if last argument is a text file 
+    // Check if last argument is a text file
     if (strcmp(argv[argc - 1], "-") == 0) {
         fd_output = STDOUT_FILENO;
     } else {
@@ -136,14 +108,6 @@ int main(int argc, char *argv[]) {
         }
     }
 
-/*
-    char **newargv = (char **)malloc(sizeof(char *) * (argc));
-
-    for (int ix = 2; ix < argc - 1; ix++) {
-        newargv[ix - 2] = (char *)argv[ix];
-    }
-    newargv[argc - 2] = NULL; */
-
     pid_t pid = fork();
 
     if (pid == 0) {
@@ -152,8 +116,7 @@ int main(int argc, char *argv[]) {
             close(fd_output);
         }
 
-       // execvp(newargv[0], newargv);
-       execve(absolute_path, words, NULL);
+        execve(absolute_path, words, NULL);
 
         printf("Exec failed\n");
         _exit(1);
